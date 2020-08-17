88341cfc357dc2d3e3e2fc697911f75cb1044f06
Vladimir <nrepublic2010@yandex.ru>
2020-08-17 19:05:25 +0300

    [+] squares func update, [+] extra else blocks rm, [+] is_prime refactor, [+] CONSTs added, [+] decorator update, [+] depth_decorator output reformat

--- homeworks/homework1.py
+++ homeworks/homework1.py
@@ -1,17 +1,17 @@
 # 1. Square func
 from functools import wraps
+from itertools import repeat
 from time import time
 
 
-def squares(*args, power=2):
+def powers(*args, power=2):
     """
     Function get list of digits, calculates the selected pow of each and return calculated list
     :param args: N integers
     :param power: power to apply
     :return: list of powered integers
     """
-    pows = [power] * len(args)
-    return list(map(pow, args, pows))
+    return list(map(pow, args, repeat(power)))
 
 
 def is_prime(num=1):
@@ -20,31 +20,35 @@ def is_prime(num=1):
     :param num: digit
     :return: true if number is prime, false otherwise
     """
-    if num == 1:
-        return False
-    for val in range(2, num):
-        if num % val == 0:
-            return False
-        elif num // val == 1:
-            return True
+    if num > 1:
+        for val in range(2, num):
+            if num % val == 0:
+                return False
+        return True
+
+    return False
+
+
+COND_EVEN = "EVEN"
+COND_ODD = "ODD"
+COND_PRIME = "PRIME"
 
 
 # 2. Digits func
-def val_filter(digits, condition="EVEN"):
+def val_filter(digits, condition=COND_EVEN):
     """
     Function filters input list of integers under specified condition
     :param digits: list of integers
     :param condition: values: EVEN, ODD, PRIME, specifying integers to find in list
     :return: list of integers after applying filter
     """
-    if condition == "EVEN":
+    if condition == COND_EVEN:
         return list(filter(lambda value: value % 2 == 0, digits))
-    elif condition == "ODD":
+    elif condition == COND_ODD:
         return list(filter(lambda value: value % 2 != 0, digits))
-    elif condition == "PRIME":
+    elif condition == COND_PRIME:
         return list(filter(is_prime, digits))
-    else:
-        return list()
+    return list()
 
 
 def timer_decorator(func):
@@ -70,16 +74,15 @@ def depth_decorator(func):
     :param func: function to trace
     :return:
     """
-    depth = 0
+    depth_decorator.depth = 0
 
     @wraps(func)
     def wrapped(*args):
-        nonlocal depth
-        print("Entering, depth:", depth)
-        depth += 1
+        print("_" * depth_decorator.depth * 4, "-->fib({})".format(*args))
+        depth_decorator.depth += 1
         res = func(*args)
-        depth -= 1
-        print("Exiting, depth:", depth)
+        depth_decorator.depth -= 1
+        print("_" * depth_decorator.depth * 4, "<--fib({})".format(*args))
         return res
 
     return wrapped
@@ -91,8 +94,7 @@ def fib(val):
         return 0
     if val == 1:
         return 1
-    else:
-        return fib(val - 1) + fib(val - 2)
+    return fib(val - 1) + fib(val - 2)
 
 
 @timer_decorator
@@ -103,7 +105,7 @@ def add(a, b):
     return res
 
 
-print(squares(1, 2, 3, power=3))
-print(val_filter([1, 2, 3, 4, 5, 6, 7], "PRIME"))
+print(powers(1, 2, 3, power=3))
+print(val_filter([1, 2, 3, 4, 5, 6, 7], COND_PRIME))
 print("Adding numbers with func, res:", add(1, 3))
 print("Fibs processing result", fib(5))
