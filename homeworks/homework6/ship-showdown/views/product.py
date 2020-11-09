b2ead7ba14ff366789e130803bb45b7c555acccc
Vladimir Chernousov <chernousovvo@nspk.ru>
2020-11-09 11:10:18 +0300

    hw6 - ship add form created

--- homeworks/homework6/ship-showdown/views/product.py
+++ homeworks/homework6/ship-showdown/views/product.py
@@ -14,6 +14,26 @@ def ship_list():
     return render_template("ships/index.html", items=ships)
 
 
+@product_app.route("/add", methods=['POST', 'GET'])
+def add_ship():
+    msg=''
+    # Insert ship into DB
+    if request.method == 'POST':
+        try: 
+            name = request.form.get('name')
+            mass = int(request.form.get('mass'))
+            speed = int(request.form.get('speed'))
+            jump = int(request.form.get('jump'))
+            img_id = int(request.form.get('img_id'))
+            s = Ship(name=name, mass=mass, speed=speed, jump=jump, img_id=img_id)
+            db.session.add(s)
+            db.session.commit()
+            msg = "Ship successfully inserted"
+        except ValueError as ex:
+            msg = ex.__str__
+    return render_template("ships/add.html", message=msg)
+
+
 @product_app.route("/<int:identifier>/", methods=['GET', 'DELETE'])
 def ship_detail(identifier: int):
     ship = Ship.query.filter_by(id=identifier).one_or_none()
