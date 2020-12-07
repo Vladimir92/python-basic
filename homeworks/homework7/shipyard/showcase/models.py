89c91428b8065f66d677433cdbc5963ea1bc24c6
Vladimir Chernousov <chernousovvo@nspk.ru>
2020-12-07 11:37:59 +0300

    Homework 7 added. Initial version

---
+++ homeworks/homework7/shipyard/showcase/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Manufacturer(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+
+class Internals(models.Model):
+    amount = models.PositiveSmallIntegerField()
+
+class Weaponry(models.Model):
+    amount = models.PositiveSmallIntegerField()
+
+class SpaceShip(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT, db_index=True)
+    mass = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
