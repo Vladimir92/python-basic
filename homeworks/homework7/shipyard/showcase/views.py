f536719f732527e77dfb9fb66486cab60539f95b
Vladimir Chernousov <chernousovvo@nspk.ru>
2020-12-14 12:56:36 +0300

    HW8 rm ununsed code

--- homeworks/homework7/shipyard/showcase/views.py
+++ homeworks/homework7/shipyard/showcase/views.py
@@ -4,13 +4,6 @@ from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-# def ship_index(request):
-#     ships = SpaceShip.objects.all()
-#     context = {
-#         'ships': ships
-#     }
-#     return render(request, 'ship_index.html', context)
-
 class ShipListView(ListView):
     model = SpaceShip
     template_name = 'ship_index.html'
@@ -19,12 +12,6 @@ class ShipListView(ListView):
         context = super().get_context_data(*args, **kwargs)
         return context
 
-# def ship_detail(request, pk):
-#     ship = SpaceShip.objects.get(pk=pk)
-#     context = {
-#         'ship': ship
-#     }
-#     return render(request, 'ship_detail.html', context)
 
 class ShipDetailView(DetailView):
     model = SpaceShip
